2d6caa1d6c765e94b8d6fd99c3317336fe74f7e3
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 20:31:12 +0200

    Ex3 added

---
+++ src/Lab1Testing/Ex3.java
@@ -0,0 +1,12 @@
+package Lab1Testing;
+
+public class Ex3 {
+    public static void main(String[]args) {
+        String s1 = "Ledri";
+        String s2 = "Vula";
+
+        boolean isEqual = s1.equals(s2);
+        System.out.println(isEqual);
+    }
+
+}
