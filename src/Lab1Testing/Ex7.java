56e7851bb56438bcda420d68ea0d1fa69cfbccf3
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 21:01:26 +0200

    Exercise 7 added

---
+++ src/Lab1Testing/Ex7.java
@@ -0,0 +1,11 @@
+package Lab1Testing;
+
+public class Ex7 {
+    public static void main(String []args){
+        for (int i=100;i>0;i--){
+            if (i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
