0c5e25055ee6d7894d95be4e6b8a3c51f70516fe
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 20:13:49 +0200

    Lab 1 package added, also ex 1

---
+++ src/Lab1Testing/Ex1.java
@@ -0,0 +1,20 @@
+public class Ex1 {
+    public static void main(String[] args) {
+
+       int a=5;
+       int b=8;
+       int c=2;
+       int result=a-b-c;
+       System.out.println("Result of a-b-c:"+result);
+
+       long d=393;
+       long e=4;
+       long f=98;
+       long result2=d*e/f;
+       System.out.println("Result of d*e/f:"+result2);
+
+
+
+
+    }
+}
