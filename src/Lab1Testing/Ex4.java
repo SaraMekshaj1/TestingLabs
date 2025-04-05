96c18b9e8e3271b7513d1ee70c6af072590ff2be
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 20:41:47 +0200

    Ex 4 added

---
+++ src/Lab1Testing/Ex4.java
@@ -0,0 +1,13 @@
+package Lab1Testing;
+
+public class Ex4 {
+    public static void main(String []args){
+        int a=3;
+        if (a%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+    }
+}
