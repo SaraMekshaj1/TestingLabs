33252cb3c49e772e02a41ce91efb850907a7fb10
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 20:46:21 +0200

    Ex5 added

---
+++ src/Lab1Testing/Ex5.java
@@ -0,0 +1,15 @@
+package Lab1Testing;
+
+public class Ex5 {
+    public static void main(String[]args){
+    int k=0;
+    if (k>0){
+        System.out.println("Value is greater than zero");
+    }
+    else if (k<0){
+        System.out.println("Value is lower than zero");
+    }
+    else {
+        System.out.println("Value is equal with zero");
+    }
+}}
