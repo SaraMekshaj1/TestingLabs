6e1fe997db3ca9ec6b970a3fa6f67766556a3cc8
Sara Mekshaj <saramekshaj19@gmail.com>
2025-04-05 20:50:29 +0200

    Ex 6 added

---
+++ src/Lab1Testing/Ex6.java
@@ -0,0 +1,16 @@
+package Lab1Testing;
+
+public class Ex6 {
+    public static void main(String []args){
+
+        for(int i=5;i<=100;i++){
+            System.out.println(i);
+        }
+        int j=5;
+        while (j<=100){
+            System.out.println(j);
+            j++;
+        }
+
+    }
+}
